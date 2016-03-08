886f4dda5256a46f8229a16bb50e448bcab373cd
Sergeij Belevskij <bbelevskij@gmail.com>
2016-03-08 09:29:10 +0500

    update deploy scripts

--- config/deploy/tests.rb
+++ config/deploy/tests.rb
@@ -1,11 +1,1 @@
-app_name = 'tests'
-server = '109.120.168.31:2298'
-user_name = 'slon'
-ssh_access_string = "#{user_name}@#{server}"
-set :stage, app_name
-set :rails_env, app_name
-
-role :app, [ssh_access_string]
-role :web, [ssh_access_string]
-role :db, [ssh_access_string]
-server server, user: user_name, roles: %w{web app db}
+/home/slon/projects/.rails_bin/config/projects/tests/config/deploy/tests.rb
